39888fb80e13f51adf9c4d4c9e664cfc61819b85
kevin <jiangtao103cp@163.com>
2016-06-11 18:58:29 +0800

    add fix

--- Android-Useful-Utils/src/main/java/org/jiangtao/android_useful_utils/view/widget/widget/FilletCustomSearchView.java
+++ Android-Useful-Utils/src/main/java/org/jiangtao/android_useful_utils/view/widget/widget/FilletCustomSearchView.java
@@ -1,32 +1,49 @@
 package org.jiangtao.android_useful_utils.view.widget.widget;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Paint;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
+import org.jiangtao.android_useful_utils.R;
 
 /**
  * Created by kevin on 16-6-10.
  */
 public class FilletCustomSearchView extends View {
+
+  private Paint mPaint;
+
   public FilletCustomSearchView(Context context) {
     super(context);
+    initialization();
   }
 
   public FilletCustomSearchView(Context context, AttributeSet attrs) {
     super(context, attrs);
+    initialization();
+    @SuppressLint("Recycle") TypedArray searchTa =
+        context.obtainStyledAttributes(attrs, R.styleable.fsv);
+    int color = searchTa.getColor(R.styleable.fsv_fsv_color,0xffff0000);
+    setBackgroundColor(color);
+    searchTa.recycle();
   }
 
   public FilletCustomSearchView(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
+    initialization();
   }
 
   @TargetApi(Build.VERSION_CODES.LOLLIPOP)
   public FilletCustomSearchView(Context context, AttributeSet attrs, int defStyleAttr,
       int defStyleRes) {
     super(context, attrs, defStyleAttr, defStyleRes);
+    initialization();
   }
 
-
+  private void initialization() {
+  }
 }
--- app/src/main/java/org/jiangtao/android/ui/activity/MainActivity.java
+++ app/src/main/java/org/jiangtao/android/ui/activity/MainActivity.java
@@ -3,6 +3,7 @@ package org.jiangtao.android.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
