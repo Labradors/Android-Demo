86d738558250a169f7f3e3321f0450ee69d375b7
= <=>
2016-08-13 02:07:50 +0800

    fix

--- app/src/main/java/org/jiangtao/android/ui/activity/PathActivity.java
+++ app/src/main/java/org/jiangtao/android/ui/activity/PathActivity.java
@@ -1,16 +1,22 @@
 package org.jiangtao.android.ui.activity;
 
+import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 import org.jiangtao.android.R;
+import org.jiangtao.android.ui.widget.HeartView;
 
 public class PathActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_path);
+        HeartView mView = new HeartView(this);
+        setContentView(mView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
 
     }
 }
